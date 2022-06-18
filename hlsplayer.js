a073d15e01f1fcfb70d5fb5adbb7f28a81a7c7cf
Solaiman Islam <71056248+Solaiman514@users.noreply.github.com>
2022-06-18 09:19:23 +0600

    Update hlsplayer.js

--- hlsplayer.js
+++ hlsplayer.js
@@ -1,7 +1,7 @@
 var url_1 = 'aHR0cDovLzEwMy41NS4xNDQuNDYvaGxzL0dhemktVFYubTN1OA==';
 var url_2 = 'aHR0cDovLzE3Mi4xNi4xMjguMjUwOjgwODEvbGl2ZS9nYXppdHYvY2h1bmtzLm0zdTg=';
 var url_3 = 'aHR0cDovLzEwMy41NS4xNDQuNDYvaGxzL3Qtc3BvcnRzLm0zdTg=';
-var url_4 = 'aHR0cHM6Ly9kYWkuZ29vZ2xlLmNvbTo0NDMvbGluZWFyL2hscy9ldmVudC9STHVLSEdHWlM3MkRzczlOUFdhbGR3L21hc3Rlci5tM3U4';
+var url_4 = 'aHR0cHM6Ly9uYm4uY2hvd2RodXJ5LXNoYWhlYi5jb20vbGl2ZS90c3BvcnRzL2NodW5rcy5tM3U4';
 var url_5 = 'aHR0cDovLzEwMy4xMzkuMTcxLjU6NTAwMS9qaW90dnBsdXMvbGl2ZS5waHA/Yz1Db2xvcnNfSEQmcT0xMjAwJmU9anVuay5tM3U4';
 
 if (Hls.isSupported()) {
